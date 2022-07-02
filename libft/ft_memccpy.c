c0c10a0891deebde30ea42ab16288305e1c031ac
nicchooyf <nicchooyf@gmail.com>
2022-07-02 17:34:12 +0800

    change test case: included original functions

--- libft/ft_memccpy.c
+++ libft/ft_memccpy.c
@@ -27,10 +27,13 @@ void *ft_memccpy(void *dest, const void * src, int c, size_t count)
 // int main () {
 //    const char src[50] = "http://www.tutorialspoint.com";
 //    char dest[50];
+//    char dest2[50];
 //    strcpy(dest,"Heloooo!!");
-//    printf("Before memcpy dest = %s\n", dest);
-//    ft_memccpy(dest, src, 'p',strlen(src)+1);
-//    printf("After memcpy dest = %s\n", dest);
+//    strcpy(dest2,"Heloooo!!");
+//    printf("Before memcpy:\ndest = %s\ndest2 = %s\n", dest, dest2);
+//    memccpy(dest, src, 'p',strlen(src)+1);
+//    ft_memccpy(dest2, src, 'p',strlen(src)+1);
+//    printf("After memcpy:\ndest = %s\ndest2 = %s\n", dest, dest2);
    
 //    return(0);
 // }
