7876ecabe855e27750e43db8cc377e7427d4a410
nicchooyf <nicchooyf@gmail.com>
2022-07-02 14:37:07 +0800

    .gitignore

---
+++ libft/ft_memset.c
@@ -0,0 +1,33 @@
+#include <stddef.h>
+
+void *ft_memset(void *str, int c, size_t n)
+{
+    int i;
+    char *p;
+    char *tmp;
+
+    p = str;
+    tmp = p;
+    i = -1;
+    while (++i < n)
+    {
+        *tmp = (char) c;
+        tmp++;
+    }
+    return p; 
+}
+
+// #include <stdio.h>
+// #include <string.h>
+
+// int main (void) {
+//    char str[50];
+
+//    strcpy(str,"This is string.h library function");
+//    puts(str);
+
+//    ft_memset(str,'$',7);
+//    puts(str);
+   
+//    return(0);
+// }
