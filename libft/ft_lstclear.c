4fde9f8b68e373e26b1537546368fdfca0ded2f5
Nicholas Choo <nchoo@u81z06s04.42kl.edu.my>
2022-07-07 21:26:09 +0800

    fix lstclear

--- libft/ft_lstclear.c
+++ libft/ft_lstclear.c
@@ -6,7 +6,7 @@
 /*   By: nchoo <nchoo@student.42kl.edu.my>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/06 20:51:09 by nchoo             #+#    #+#             */
-/*   Updated: 2022/07/06 21:03:37 by nchoo            ###   ########.fr       */
+/*   Updated: 2022/07/07 21:18:32 by nchoo            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,15 @@
 
 void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
+	t_list	*tmp;
+
 	if (!lst)
 		return ;
 	while (*lst)
 	{
+		tmp = (*lst)->next;
 		(*del)((*lst)->content);
 		free(*lst);
-		*lst = (*lst)->next;
+		*lst = tmp;
 	}
 }
